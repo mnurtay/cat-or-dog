6d3aeacc7090acf3ab93eac1d6a3acd187701552
nurtaims <nurtaims@gmail.com>
2019-04-01 18:59:07 +0600

    Structure created

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Font} from 'expo'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+import Reducer from './src/reducer'
+import Holder from './src/component/HolderComponent'
+import ClassifyContainer from './src/container/ClassifyContainer'
+
+const middleware = applyMiddleware(thunk)
+const store = createStore(Reducer, middleware)
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { fontLoaded: false }
+  }
+
+  async componentDidMount(){
+    await Font.loadAsync({
+      'sf-regular': require('./assets/fonts/SFProText-Regular.ttf'),
+      'sf-light': require('./assets/fonts/SFProText-Light.ttf'),
+      'sf-medium': require('./assets/fonts/SFProText-Medium.ttf'),
+      'sf-bold': require('./assets/fonts/SFProText-Bold.ttf'),
+    });
+    this.setState({ fontLoaded: true })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <Holder isLoading={!this.state.fontLoaded}>
+          <ClassifyContainer />
+        </Holder>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/component/HolderComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import {MAIN_COLOR_3, MAIN_BACKGROUND} from '../../utils/constants'
+
+class Holder extends Component {
+    render(){
+        return(
+            <View style={styles.holder}>
+                {
+                    this.props.isLoading
+                    ? <View style={styles.innerHolder}>
+                        <ActivityIndicator size='large' color={MAIN_COLOR_3}/>
+                    </View>
+                    : this.props.children
+                }
+            </View>
+        )
+    }
+}
+
+Holder.defaultProps = {
+    isLoading: false
+}
+
+export default Holder;
+
+const styles = StyleSheet.create({
+    holder: {
+        flex: 1
+    },
+    innerHolder: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: MAIN_BACKGROUND
+    }
+})
